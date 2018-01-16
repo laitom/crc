0b4a3ad308b1e5d0411cdf273ec7d7d7a19949a4
Tommi Laivamaa <laivtom@gmail.com>
2018-01-16 19:21:39 +0200

    Added lookup table optimization option

--- crc16.h
+++ crc16.h
@@ -41,11 +41,31 @@
 static uint16_t __crc16(uint8_t *msg, size_t len, uint16_t poly, uint16_t init, uint16_t xorout) {
     uint16_t ret = init;
 
+#ifndef CRC16_USE_LOOKUP_TABLE
     for (int i = 0; i < len; ++i) {
 	ret ^= *(msg+i) << 8;
 	for (int j = 0; j < 8; ++j)
 	    ret = ((ret & 0x8000) != 0) ? ((ret << 1) ^ poly) : ret << 1;
     }
+#else
+    uint16_t table[256] = { 0 };
+    uint16_t temp;
+
+    if (table[1] == 0) {
+	for (int i = 0; i < 256; ++i) {
+	    temp = (uint16_t) (i << 8);
+	    
+	    for (int j = 0; j < 8; ++j)
+		temp = ((temp & 0x8000) != 0) ? ((temp << 1) ^ poly) : temp << 1;
+	    
+	    table[i] = temp;
+	}
+    }
+
+    for (int i = 0; i < len; ++i) {
+	ret = (ret << 8) ^ table[(ret >> 8) ^ *(msg+i)];
+    }
+#endif
 
     return ret ^ xorout;
 }
