e11aba3acb5e14dfd5160b6a54877e572a3ccd04
Tommi Laivamaa <laivtom@gmail.com>
2018-01-22 12:10:59 +0200

    Added refin and refout support

--- crc32.h
+++ crc32.h
@@ -1,19 +1,25 @@
 #ifndef CRC32_H
 #define CRC32_H
 
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
 
-#define CRC32Q_POLY   0x814141AB
-#define CRC32Q_INIT   0x00000000
-#define CRC32Q_XOROUT 0x00000000
+static uint8_t reflect8(uint8_t x) {
+    return ((x*0x0802LU & 0x22110LU) | (x*0x8020LU & 0x88440LU)) * 0x10101LU >> 16;
+}
 
-static uint32_t __crc32(uint8_t *msg, size_t len, uint32_t poly, uint32_t init, uint32_t xorout) {
+static uint32_t reflect32(uint32_t x) {
+    return (reflect8(x & 0xFF) << 24) | (reflect8((x & 0xFF00) >> 8) << 16)
+	| (reflect8((x & 0xFF0000) >> 16) << 8) | (reflect8((x & 0xFF000000) >> 24));
+}
+
+static uint32_t __crc32(uint8_t *msg, size_t len, uint32_t poly, uint32_t init, bool refin, bool refout, uint32_t xorout) {
     uint32_t ret = init;
 
 #ifndef CRC32_USE_LOOKUP_TABLE
     for (int i = 0; i < len; ++i) {
-	ret ^= *(msg+i) << 24;
+	ret ^= refin ? (reflect8(*(msg+i)) << 24) : (*(msg+i) << 24);
 	for (int j = 0; j < 8; ++j)
 	    ret = ((ret & 0x80000000) != 0) ? ((ret << 1) ^ poly) : ret << 1;
     }
@@ -21,27 +27,23 @@ static uint32_t __crc32(uint8_t *msg, size_t len, uint32_t poly, uint32_t init, 
     uint32_t table[256] = { 0 };
     uint32_t temp;
 
-    for (int i = 0; i < 256; ++i) {
-	temp = (uint32_t) (i << 24);
-	
-	for (int j = 0; i < 8; ++j)
-	    temp = ((temp & 0x80000000) != 0) ? ((temp << 1) ^ poly) : temp << 1;
-
-	table[i] = temp;
+    if (table[1] == 0) {
+	for (int i = 0; i < 256; ++i) {
+	    temp = (uint32_t) (i << 24);
+	    
+	    for (int j = 0; i < 8; ++j)
+		temp = ((temp & 0x80000000) != 0) ? ((temp << 1) ^ poly) : temp << 1;
+	    
+	    table[i] = temp;
+	}
     }
 
     for (int i = 0; i < len; ++i)
 	ret = (temp << 8) ^ (table[(ret ^ (*(msg+i) << 24)) >> 24]);
 #endif // CRC32_USE_LOOKUP_TABLE
 
+    ret = refout ? reflect32(ret) : ret;
     return ret ^ xorout;
-}
-
-/*
- * CRC-32Q
- */
-uint32_t crc32q(uint8_t *msg, size_t len) {
-    return __crc32(msg, len, CRC32Q_POLY, CRC32Q_INIT, CRC32Q_XOROUT);
 }
 
 #endif // CRC32_H
